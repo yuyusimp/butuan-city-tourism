76af50ebab92306a1a70b997c7a2c1f9507fe8a9
Christian yandug <cyandug2@gmail.com>
2024-12-20 14:27:08 +0800

    git commit -m

--- js/seemore.js
+++ js/seemore.js
@@ -1,24 +1,21 @@
-    document.addEventListener("DOMContentLoaded", () => {
-        const testimonials = document.querySelectorAll(".testimonial-item");
-        
-        testimonials.forEach(item => {
-            const content = item.querySelector("div.border-top");
+    document.addEventListener("DOMContentLoaded", function () {
+        const testimonialItems = document.querySelectorAll(".testimonial-item");
+
+        testimonialItems.forEach((item) => {
             const seeMoreBtn = document.createElement("button");
-            seeMoreBtn.classList.add("see-more-btn");
-            seeMoreBtn.innerText = "See More";
+            seeMoreBtn.className = "see-more-btn";
+            seeMoreBtn.textContent = "See More";
+            item.appendChild(seeMoreBtn);
 
-            if (content.scrollHeight > 450) {
-                item.appendChild(seeMoreBtn);
-
-                seeMoreBtn.addEventListener("click", () => {
-                    if (item.classList.contains("expanded")) {
-                        item.classList.remove("expanded");
-                        seeMoreBtn.innerText = "See More";
-                    } else {
-                        item.classList.add("expanded");
-                        seeMoreBtn.innerText = "See Less";
-                    }
-                });
-            }
+            seeMoreBtn.addEventListener("click", function () {
+                if (item.classList.contains("expanded")) {
+                    item.classList.remove("expanded");
+                    seeMoreBtn.textContent = "See More"; // Switch to "See More"
+                } else {
+                    item.classList.add("expanded");
+                    seeMoreBtn.textContent = "See Less"; // Switch to "See Less"
+                }
+            });
         });
     });
+
